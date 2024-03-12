2187999c0713dd603940748a7a2e22edff044f01
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-03-12 21:28:42 +0500

    Day 04 of Coding

---
+++ day04.js
@@ -0,0 +1,14 @@
+//Q10
+// Mehak Hamid , 12-03-2024
+var Name = "Mehak Hamid";
+console.log("Hello Everyone its ".concat(Name, " here"));
+//Q11
+var Name1 = ["Jugnu", "Zainab"];
+for (var i = 0; i < Name1.length; i++) {
+    console.log(Name1[i]);
+}
+//Q12
+for (var _i = 0, Name1_1 = Name1; _i < Name1_1.length; _i++) {
+    var Name_1 = Name1_1[_i];
+    console.log("Hello MyBFF ".concat(Name1, ", how are you?"));
+}
