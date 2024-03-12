c0931aee0f181e8fcc5eda984150aa809f6a8b56
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-03-12 20:42:39 +0500

    Day 02 of Coding

---
+++ day02.js
@@ -0,0 +1,11 @@
+// Q1
+console.log("Albert Einstein once said ,\"A person who never made a mistake will never tried antything new\"");
+//Q2
+var famous_Person = "Albert Einstein";
+var qoute = "A person who never made a mistake will never tried antything new";
+var Message = "".concat(famous_Person, " once said ,").concat(qoute);
+console.log("".concat(Message));
+//Q3
+var Name = "\t\n Asharib \t\n";
+console.log(Name);
+console.log(Name.trim());
