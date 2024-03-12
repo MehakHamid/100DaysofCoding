1cce1cc83159489a549248630258debf0e1d2904
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-03-12 20:41:21 +0500

    Day 01 of Coding

---
+++ day01.js
@@ -0,0 +1,8 @@
+//Q2
+var PersonName = "Asharib";
+console.log("Hello ".concat(PersonName, ", would you like to learn some typescript today?"));
+//Q3
+var Name = "Pakistan";
+console.log(Name.toLocaleLowerCase());
+console.log(Name.toLocaleUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
