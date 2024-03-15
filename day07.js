7f5befcc7a316c4ca53b3f4b5bb1543dc9d49ee4
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-03-15 10:06:28 +0500

    Day 07 of Coding

---
+++ day07.js
@@ -0,0 +1,14 @@
+//Q19
+//Number of inviting guest
+var Friends = ["Zainab", "Soniya", "Jugnu"];
+console.log("Dear ".concat(Friends.length, " i'm inviting you for a birthday party"));
+//Q20
+var MHfarm = ["Wild Cats", "Dogs", "Birds", "ETC"];
+console.log("I want to create My Farm with different animals ".concat(MHfarm));
+//Q21
+var Book = {
+    title: "MH Farm",
+    Author: "Mehak Hamid",
+    Yearpublised: 2026
+};
+console.log("book info: ".concat(Book.title, " by ").concat(Book.Author, " Published in ").concat(Book.Yearpublised));
