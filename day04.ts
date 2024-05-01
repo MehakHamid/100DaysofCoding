3458cdf94e8857c669391cb51d04c8e8a042b3bc
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-05-01 10:43:28 +0500

    Day 04 of Coding

---
+++ day04.ts
@@ -0,0 +1,12 @@
+//Q10
+// Mehak Hamid , 12-03-2024
+let Name ="Mehak Hamid"
+console.log(`Hello Everyone its ${Name} here`)
+
+//Q11
+let Name1: string[] = ["Jugnu","Zainab"]
+for (let i = 0; i < Name1.length; i++){console.log(Name1[i])}
+
+//Q12
+
+for(let Name of Name1){console.log(`Hello MyBFF ${Name1}, how are you?`)}
