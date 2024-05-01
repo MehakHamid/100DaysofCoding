778e2020f35c3619da5da056600de15094f6bac0
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-05-01 10:41:08 +0500

    day01 of coding

---
+++ day01.ts
@@ -0,0 +1,9 @@
+//Q2
+let PersonName: string= "Asharib"
+console.log(`Hello ${PersonName}, would you like to learn some typescript today?`)
+
+//Q3
+let Name = "Pakistan"
+console.log(Name.toLocaleLowerCase())
+console.log(Name.toLocaleUpperCase())
+console.log(Name.charAt(0).toUpperCase() +Name.slice(1).toLowerCase())
