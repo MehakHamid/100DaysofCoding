4286a3c64f2892c20a439a4bf810d812bc6cee04
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-05-01 10:45:11 +0500

    Day 07 of Coding

---
+++ day07.ts
@@ -0,0 +1,18 @@
+//Q19
+//Number of inviting guest
+
+let Friends: string[] = ["Zainab", "Soniya", "Jugnu"]
+console.log(`Dear ${Friends.length} i'm inviting you for a birthday party`)
+
+//Q20
+let MHfarm: string[] = ["Wild Cats" , "Dogs" , "Birds" , "ETC"]
+console.log(`I want to create My Farm with different animals ${MHfarm}`)
+
+//Q21
+let Book: {title: String; Author: string; Yearpublised: number} =
+{
+    title: "MH Farm",
+ Author: "Mehak Hamid",
+ Yearpublised: 2026
+};
+console.log(`book info: ${Book.title} by ${Book.Author} Published in ${Book.Yearpublised}`)
