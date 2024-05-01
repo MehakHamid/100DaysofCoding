a027a9cb704b8687c03d7209a7b6b4eff003ed35
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-05-01 11:26:06 +0500

    Day 09 of coding

---
+++ day09.js
@@ -0,0 +1,18 @@
+//Question 25: Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+var alien_color = 'green';
+if (alien_color == "green") {
+    console.log("you earned 10 points");
+}
+if (alien_color == "red") {
+}
+//Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+var alienColour = "";
+if (alienColour == "green") {
+    console.log("you earned 10 points");
+}
+else if (alienColour == "red") {
+    console.log("you earned 5 points");
+}
+else {
+    console.log("you earned 0 points");
+}
