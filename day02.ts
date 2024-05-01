bf506842fa9b41e8033ef00c0428ed9769686ff2
Mehak Hamid <155909983+MehakHamid@users.noreply.github.com>
2024-05-01 10:42:02 +0500

    Day 02 of Coding

---
+++ day02.ts
@@ -0,0 +1,18 @@
+// Q1
+
+console.log(`Albert Einstein once said ,
+"A person who never made a mistake 
+will never tried antything new"`)
+
+//Q2
+
+let famous_Person = "Albert Einstein"
+let qoute = "A person who never made a mistake will never tried antything new"
+let Message = `${famous_Person} once said ,${qoute}`
+console.log(`${Message}`)
+
+//Q3
+let Name : string = "\t\n Asharib \t\n";
+console.log(Name)
+console.log(Name.trim())
+
